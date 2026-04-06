2e88926382293400a5878bebbfa25d0e4adc8e8a
Tash518 <158076683+Tash518@users.noreply.github.com>
2026-04-06 23:47:44 +0530

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+       Stack<Integer> record = new Stack<>();
+       for(String c : operations){
+        int size = record.size();
+        if(c.equals("+")){
+            int a=record.pop();
+            int b=record.peek();
+            record.push(a);
+            record.push(a+b);
+        }
+        else if(c.equals("D")){
+            record.push(2*record.peek());
+        }
+        else if(c.equals("C")){
+            record.pop();
+        }
+        else{
+            record.add(Integer.parseInt(c));
+        }
+       }
+       int sum=0;
+       for(int i : record){
+            sum+=i;
+       }
+       return sum;
+    }
+}
