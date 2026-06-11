87c1a8e9685cd418230942e18bc8b91a92bba9c2
Tash518 <158076683+Tash518@users.noreply.github.com>
2026-06-11 13:29:53 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    private int[] dp;
+    private int solve(int n){
+        if(n<=1) return 1;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = solve(n-1)+solve(n-2);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n);
+    }
+}
